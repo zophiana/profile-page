d4f05886ecb5d0444c9bb1feaca17f8baa0a4e44
zophiana <53372671+zophiana@users.noreply.github.com>
2024-06-10 17:09:33 +0200

    footer: update copyright

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -7,7 +7,7 @@ type FooterProps = {
 export default function Footer(props: FooterProps) {
   return (
     <footer className={`py-2 ${props.className}`}>
-      <Clamp>Made by Leonard Schwennesen 2023 &copy;</Clamp>
+      <Clamp>Made by Leonard Schwennesen 2024 &copy;</Clamp>
     </footer>
   )
 }
