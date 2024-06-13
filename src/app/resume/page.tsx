5f0d718c0eec7e799157ba338a8e569a3e07d3ca
zophiana <53372671+zophiana@users.noreply.github.com>
2024-06-13 16:49:57 +0200

    resume: refactor card

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -27,26 +27,6 @@ function ResumeDate(props: { date: string }) {
   )
 }
 
-type ResumeCardProps = {
-  title?: string
-  description?: string
-  children?: React.ReactNode
-}
-
-function ResumeCard(props: ResumeCardProps) {
-  return (
-    <div className="py-2">
-      <Card className="rounded-3xl">
-        <CardHeader className="pb-3">
-          <CardTitle>{props.title}</CardTitle>
-          <CardDescription>{props.description}</CardDescription>
-        </CardHeader>
-        <CardContent>{props.children}</CardContent>
-      </Card>
-    </div>
-  )
-}
-
 export default function CVPage() {
   return (
     <main className="flex-auto">
@@ -56,60 +36,107 @@ export default function CVPage() {
           <div className="w-32 gradient-mask-t-0">
             <div className="h-24 w-1/2 border-r"></div>
           </div>
-          <div></div>
 
           <ResumeDate date="Aug. 2023 - laufend"></ResumeDate>
           <DotWithLine></DotWithLine>
-          <ResumeCard
-            title="Ausbildung zum Fachinformatiker für Anwendungsentwicklung"
-            description="Rhenus Freight Network GmbH, Rhenus-Platz 1, 59439 Holzwickede"
-          >
-            <ul className="list-inside list-disc pl-1">
-              <li>Arbeit in einem Software-Entwicklungsteam</li>
-              <li>IntelliJ mit Java und Scout-Framework</li>
-              <li>Oracle SQL und Oracle PL/SQL</li>
-              <li>Jira und Confluence</li>
-              <li>GitLab Entwicklungs-Workflow</li>
-            </ul>
-          </ResumeCard>
+          <div className="col-span-2 py-2">
+            <Card className="rounded-3xl">
+              <CardHeader className="pb-3">
+                <CardTitle>
+                  Ausbildung zum Fachinformatiker
+                  <br />
+                  für Anwendungsentwicklung
+                </CardTitle>
+                <CardDescription>
+                  Rhenus Freight Network GmbH,&nbsp;
+                  <br className="block lg:hidden" />
+                  Rhenus-Platz 1, 59439 Holzwickede
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul className="list-inside list-disc pl-1">
+                  <li>Arbeit in einem Software-Entwicklungsteam</li>
+                  <li>IntelliJ mit Java und Scout-Framework</li>
+                  <li>Oracle SQL und Oracle PL/SQL</li>
+                  <li>Jira und Confluence</li>
+                  <li>GitLab Entwicklungs-Workflow</li>
+                </ul>
+              </CardContent>
+            </Card>
+          </div>
 
           <ResumeDate date="Nov. 2023 - laufend"></ResumeDate>
           <DotWithLine></DotWithLine>
-          <ResumeCard
-            title="Nebenjob: Systemverwaltung und -wartung"
-            description="Umweltzentrum Westfalen gGmbH, Westenhellweg 110, 59192 Bergkamen"
-          >
-            <ul className="list-inside list-disc pl-1">
-              <li>Verwaltung von Linux-Servern (Nextcloud, Docker, Ansible)</li>
-              <li>Verwaltung von Windows 10/11</li>
-            </ul>
-          </ResumeCard>
+          <div className="col-span-2 py-2">
+            <Card className="rounded-3xl">
+              <CardHeader className="pb-3">
+                <CardTitle>
+                  Nebenjob: Systemverwaltung
+                  <br />
+                  und -wartung
+                </CardTitle>
+                <CardDescription>
+                  Umweltzentrum Westfalen gGmbH,&nbsp;
+                  <br className="block lg:hidden" />
+                  Westenhellweg 110, 59192 Bergkamen
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul className="list-inside list-disc pl-1">
+                  <li>
+                    Verwaltung von Linux-Servern (Nextcloud,
+                    <br />
+                    Docker, Ansible)
+                  </li>
+                  <li>Verwaltung von Windows 10/11</li>
+                </ul>
+              </CardContent>
+            </Card>
+          </div>
 
           <ResumeDate date="Aug. 2022 - Juli 2023"></ResumeDate>
           <DotWithLine></DotWithLine>
-          <ResumeCard
-            title="Freiwilliges Ökologisches Jahr (FÖJ)"
-            description="Umweltzentrum Westfalen gGmbH, Westenhellweg 110, 59192 Bergkamen"
-          >
-            <ul className="list-inside list-disc pl-1">
-              <li>Landessprecher des FÖJ in NRW</li>
-              <li>Einsatz für die Interessen der Freiwilligen</li>
-              <li>Organisation von Treffen mit LWL-Landesrätin</li>
-            </ul>
-          </ResumeCard>
+          <div className="col-span-2 py-2">
+            <Card className="rounded-3xl">
+              <CardHeader className="pb-3">
+                <CardTitle>Freiwilliges Ökologisches Jahr (FÖJ)</CardTitle>
+                <CardDescription>
+                  Umweltzentrum Westfalen gGmbH,&nbsp;
+                  <br className="block lg:hidden" />
+                  Westenhellweg 110, 59192 Bergkamen
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul className="list-inside list-disc pl-1">
+                  <li>Landessprecher des FÖJ in NRW</li>
+                  <li>Einsatz für die Interessen der Freiwilligen</li>
+                  <li>Organisation von Treffen mit LWL-Landesrätin</li>
+                </ul>
+              </CardContent>
+            </Card>
+          </div>
 
           <ResumeDate date="Aug. 2013 - Juli 2022"></ResumeDate>
           <DotWithLine></DotWithLine>
-          <ResumeCard
-            title="Abitur"
-            description="Werner-von-Siemens-Gesamtschule Königsborn, Döbelner Str. 7, 59425 Unna"
-          >
-            <ul className="list-inside list-disc pl-1">
-              <li>Mathe und Technik als Leistungskurse</li>
-              <li>Geschichte und Englisch als Grundkurse</li>
-              <li>Physik und Chemie als reguläre Fächer</li>
-            </ul>
-          </ResumeCard>
+          <div className="col-span-2 py-2">
+            <Card className="rounded-3xl">
+              <CardHeader className="pb-3">
+                <CardTitle>Abitur</CardTitle>
+                <CardDescription>
+                  Werner-von-Siemens-Gesamtschule Königsborn,&nbsp;
+                  <br className="block lg:hidden" />
+                  Döbelner Str. 7, 59425 Unna
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul className="list-inside list-disc pl-1">
+                  <li>Mathe und Technik als Leistungskurse</li>
+                  <li>Geschichte und Englisch als Grundkurse</li>
+                  <li>Physik und Chemie als reguläre Fächer</li>
+                </ul>
+              </CardContent>
+            </Card>
+          </div>
 
           <div></div>
           <div className="w-32 gradient-mask-b-0">
